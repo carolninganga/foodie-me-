30d80a2d9115f7a532b190629fabd5a65108de3d
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-11 18:07:13 -0400

    added card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Search from './components/Search/Search';
+import Result from './components/Result/Result';
+import API from './utils/API'
 import './App.css';
+class App extends Component {
+  state = {
+    result: [],
+    term: "",
+    location:""
+  };
+  componentDidMount() {
+    this.searchStore('coffee',"New York");
+  }
+  searchStore = (term,location) => {
+    API.search(term,location)
+    .then(res => console.log(res.data.businesses))
+      .then(res => this.setState({ result: res.data.businesses })) 
+      .catch(err => console.log(err));
+  };
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState ({
+      [name]: value
+    });
+  };
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.searchStore(this.state.term,this.state.location);
+  }
+  render() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="container h-100">  
+      <div className="row h-100 justify-content-center align-items-center">
+        <form className="col-10">
+        <Search 
+                term={this.state.term}
+                location={this.state.location}
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}/>
+        <Result 
+          result = {this.state.result}
+        />
+        </form>
+      </div>
+      </div>
+    )
+  }
 }
-
 export default App;
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -8,13 +8,25 @@ function Result(props) {
         <div id="article-section" className="card-body">
         {props.result ? props.result.map((item) => { 
           return(
-              <li className='list-group-item' key={item.id}>
-              <h5>{item.name}</h5>
-              <p>Phone: {item.display_phone}</p>
-              <p>Location: {item.location.address1}, {item.location.city}, {item.location.state} {item.location.zip_code}</p>
-              <p>Rating:{item.rating}
-              <a href={item.url}>View More on Yelp</a></p>
-              </li>  
+            //   <Card className='list-group-item' key={item.id}>
+            //   <h5>{item.name}</h5>
+            //   <p>Phone: {item.display_phone}</p>
+            //   <p>Location: {item.location.address1}, {item.location.city}, {item.location.state} {item.location.zip_code}</p>
+            //   <p>Rating:{item.rating}
+            //   <a href={item.url}>View More on Yelp</a></p>
+            //   </li>
+
+                <div className="card" style={{width: "18rem"}}>
+                    <img src="" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                        <h5 className="card-title">{item.name}</h5>
+                        <p className="card-text">{item.location.city}</p>
+                        <p className="card-text">{item.location.zip_code}</p>
+                    <a href={item.url} className="btn btn-primary">View More on Yelp</a>
+                </div>
+                </div>
+
+
             )
           }): <p>No stores available!</p>}
         </div>
