729ee359b0e68cbc2a1949d1c8dd5db6389bed42
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-11 20:15:07 -0400

    added display for phone number

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -26,8 +26,7 @@ function Result(props) {
                     <img src={item.image_url} className="card-img-top" alt="businessImage" style={{width:"100%", height:"250px"}} />
                         <div className="card-body">
                         <h5 className="card-title">{item.name}</h5>
-                        <p>{item.phone}</p>
-
+                        <p>Phone: {item.display_phone}</p>
                         <p className="card-text">{item.location.address1} {item.location.city} {item.location.state} {item.location.zip_code}</p>
                         {/* <p className="card-text">{</p> */}
                         <span>Rating:{item.rating}</span>{"   "}<a href={item.url} className="btn btn-primary"><span>View More on Yelp</span></a>
