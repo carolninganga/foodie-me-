dbff7e4faac60a58625037fa59b8ec5e1bf52b03
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-11 20:15:57 -0400

    added rating class name

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -29,7 +29,7 @@ function Result(props) {
                         <p>Phone: {item.display_phone}</p>
                         <p className="card-text">{item.location.address1} {item.location.city} {item.location.state} {item.location.zip_code}</p>
                         {/* <p className="card-text">{</p> */}
-                        <span>Rating:{item.rating}</span>{"   "}<a href={item.url} className="btn btn-primary"><span>View More on Yelp</span></a>
+                        <span className="rating">Rating:{item.rating}</span>{"   "}<a href={item.url} className="btn btn-primary"><span>View More on Yelp</span></a>
                       </div>
                 </div>
             )
